9c9e93916af675268a8c37b99478e92dd4478901
alexandre.denault@gmail.com <alexandre.denault@gmail.com@e1c9e452-2ef9-1ea6-95f3-d23a60559cc5>
2011-05-11 02:50:37 +0000

    First version of Java Rouge driver.

---
+++ trunk/driver/java/src/ca/qc/adinfo/rouge/bencode/BEncodeChannelHandler.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright [2011] [ADInfo, Alexandre Denault]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.qc.adinfo.rouge.bencode;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+
+import ca.qc.adinfo.rouge.RougeDriver;
+import ca.qc.adinfo.rouge.data.RougeObject;
+
+public class BEncodeChannelHandler extends SimpleChannelUpstreamHandler {
+
+	private static Logger log = Logger.getLogger(BEncodeChannelHandler.class);
+
+	private RougeDriver driver;
+
+	public BEncodeChannelHandler(RougeDriver driver) {
+		this.driver = driver;
+
+	}
+
+	@Override
+	public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) {
+
+		log.trace("Channel connected " + e.getChannel().getId());
+
+		driver.channel = e.getChannel();
+		if (this.driver.listener != null) {
+			driver.listener.onConnect();
+		}
+
+	}
+
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+
+		log.trace("Message received from channel " + e.getChannel().getId());
+
+		ChannelBuffer channelBuffer = (ChannelBuffer)e.getMessage();
+		
+		RougeObject resp = (RougeObject)BDecoder.bdecode(
+				new ByteArrayInputStream(channelBuffer.array())).getValue();
+
+		String command = resp.getString("command"); 
+		RougeObject payload = resp.getNovaObject("payload");
+
+		if (this.driver.listener != null) {
+			this.driver.listener.onMessage(command, payload);
+		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+		// Close the connection when an exception is raised.
+		log.error("Unexpected exception from downstream.");
+		e.getCause().printStackTrace();
+		e.getChannel().close();
+	}
+}
---
+++ trunk/driver/java/src/ca/qc/adinfo/rouge/bot/RougeBot.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright [2011] [ADInfo, Alexandre Denault]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.qc.adinfo.rouge.bot;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Random;
+
+import org.apache.log4j.Logger;
+
+import ca.qc.adinfo.rouge.RougeDriver;
+import ca.qc.adinfo.rouge.RougeListener;
+import ca.qc.adinfo.rouge.data.RougeObject;
+
+public class RougeBot implements RougeListener, Runnable  {
+
+	private final static Logger log = Logger.getLogger(RougeTest.class);
+
+	private RougeDriver driver;
+	private String roomName;
+	private boolean ready;
+	
+
+	public RougeBot() {
+
+		try {
+		    InetAddress addr = InetAddress.getLocalHost();
+
+		    // Get IP Address
+		    byte[] ipAddr = addr.getAddress();
+
+		    // Get hostname
+		    this.roomName = addr.getHostName();
+		} catch (UnknownHostException e) {
+			
+			this.roomName = "Unknown";
+		}
+	}
+
+	@Override
+	public void onMessage(String command, RougeObject payload) {
+
+		System.out.println(command + " " + payload.toJSON().toString());
+
+		if (command.equals("login")) {
+
+			RougeObject newPayload = new RougeObject();
+			newPayload.putString("name", roomName);
+			this.driver.execute("room.create", newPayload);
+			this.driver.execute("room.join", newPayload);
+			
+		} else if (command.equals("room.join")) {
+			
+			this.ready = true;
+		}
+	}
+
+	@Override
+	public void onConnect() {
+
+		log.trace("Connected ...");
+
+		RougeObject payload = new RougeObject();
+		payload.putString("username", roomName + System.currentTimeMillis());
+		payload.putString("password", "password");
+
+		this.driver.execute("login", payload);
+	}
+
+	@Override
+	public void run() {
+
+		Random random = new Random();
+
+		while(true) {
+
+			this.ready = false;
+
+			this.driver = new RougeDriver("localhost", 6612, this, true);
+			this.driver.connect();
+
+			int rounds = random.nextInt(200) + 5;
+
+			for(int i = 0; i < rounds; i++) {
+
+				try {
+					Thread.sleep(2000 + random.nextInt(8000));
+				} catch (InterruptedException e) {
+
+				}
+
+				if (this.ready) {
+
+					RougeObject payload = new RougeObject();
+					payload.putString("name", this.roomName);
+					payload.putString("message", "hello");
+					this.driver.execute("room.say", payload);
+				}
+			}
+
+			this.driver.disconnect();
+		}
+	}
+
+	@Override
+	public void onDisconnect() {
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		
+		for(int i = 0; i < 20; i++) {
+			
+			Thread t = new Thread(new RougeBot());
+			t.start();
+			
+			try {
+				Thread.sleep(random.nextInt(5000));
+			} catch (InterruptedException e) {
+
+			}
+		}
+
+	}
+
+
+
+}
