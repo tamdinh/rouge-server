245cdf2ea3ed3c2bffc1942fbc9db573e852585d
alexandre.denault <alexandre.denault@gmail.com>
2011-05-11 02:32:28 +0000

    First commit of Rouge Server

---
+++ server/src/ca/qc/adinfo/rouge/data/RougeArray.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright [2011] [ADInfo, Alexandre Denault]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.qc.adinfo.rouge.data;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.sf.json.JSONArray;
+
+public class RougeArray {
+
+	public LinkedList<RougeDataWrapper> content;
+	
+	public RougeArray() {
+		
+		this.content = new LinkedList<RougeDataWrapper>();
+	}
+	
+	public RougeArray(JSONArray jArray) {
+		
+		this.content = new LinkedList<RougeDataWrapper>();
+		
+		for (int i = 0; i < jArray.size(); i++) {
+			this.content.add(new RougeDataWrapper(jArray.get(i)));
+		}
+	}
+	
+	public RougeArray(Collection col) {
+		
+		this.content = new LinkedList<RougeDataWrapper>();
+		
+		for(Object o: col) {
+			
+			this.content.add(new RougeDataWrapper(o));
+		}
+	}
+	
+	public int getInt(int index) {
+		return (Integer)this.content.get(index).getValue();
+	}
+	
+	public long getLong(int index) {
+		return (Long)this.content.get(index).getValue();
+	}
+	
+	public String getString(int index) {
+		return (String)this.content.get(index).getValue();
+	}
+	
+	public boolean getBoolean(int index) {
+		int b = (Integer)this.content.get(index).getValue();
+		return (b == 1);
+	}
+	
+	public float getFloat(int index) {
+		return (Float)this.content.get(index).getValue();
+	}
+	
+	public double getDouble(int index) {
+		return (Double)this.content.get(index).getValue();
+	}
+	
+	public RougeArray getNovaArray(int index) {
+		return (RougeArray)this.content.get(index).getValue();
+	}
+	
+	public RougeObject getNovaObject(int index) {
+		return (RougeObject)this.content.get(index).getValue();
+	}
+	
+	public void add(Object value) {
+		this.content.add(new RougeDataWrapper(value));
+	}
+	
+	public void addInt(int value) {
+		this.content.add(new RougeDataWrapper(value));
+	}
+	
+	public void addLong(Long value) {
+		this.content.add(new RougeDataWrapper(value));
+	}
+	
+	public void addString(String value) {
+		this.content.add(new RougeDataWrapper(value));
+	}
+	
+	public void addBoolean(boolean value) {
+		if (value) {
+			this.content.add(new RougeDataWrapper(1));
+		} else {
+			this.content.add(new RougeDataWrapper(0));
+		}
+		
+	}
+	
+	public void addFloat(float value) {
+		this.content.add(new RougeDataWrapper(value));
+	}
+	
+	public void addDouble(double value) {
+		this.content.add(new RougeDataWrapper(value));
+	}
+	
+	public void addNovaArray(RougeArray value) {
+		this.content.add(new RougeDataWrapper(value));
+	}
+	
+	public void addNovaObject(RougeObject value) {
+		this.content.add(new RougeDataWrapper(value));
+	}
+	
+	public JSONArray toJSON() {
+		
+		JSONArray jArray = new JSONArray();
+		
+		for(RougeDataWrapper data: this.content) {
+
+			Object obj = data.getValue();
+			
+			if (obj instanceof RougeObject) {
+				jArray.add( ((RougeObject)obj).toJSON() );
+			} else if (obj instanceof RougeArray) {
+				jArray.add( ((RougeArray)obj).toJSON() );
+			} else {
+				jArray.add(obj);
+			}
+		}
+		
+		return jArray;
+	}
+	
+	public List toList() {
+	
+		List list = new ArrayList();
+		
+		for(RougeDataWrapper data: this.content) {
+
+			Object obj = data.getValue();
+			
+			if (obj instanceof RougeObject) {
+				list.add( ((RougeObject)obj).toMap() );
+			} else if (obj instanceof RougeArray) {
+				list.add( ((RougeArray)obj).toList() );
+			} else {
+				list.add(obj);
+			}
+		}
+		
+		return list;
+		
+	}
+	
+}
---
+++ server/src/ca/qc/adinfo/rouge/server/core/bencode/BDecoder.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright [2011] [ADInfo, Alexandre Denault]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.qc.adinfo.rouge.server.core.bencode;
+
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
+
+import ca.qc.adinfo.rouge.data.RougeDataWrapper;
+
+public class BDecoder {
+
+	private static Logger log = Logger.getLogger(BDecoder.class);
+	
+	public static RougeDataWrapper bdecode(InputStream in) {
+		
+		return null;
+	}
+
+}
