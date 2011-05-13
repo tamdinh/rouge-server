6cd3300d60435f80b83cd856ecd606c5ad5c29a5
alexandre.denault <alexandre.denault@gmail.com>
2011-05-13 17:07:19 +0000

    Refactored how pages are loaded and written.

---
+++ server/src/ca/qc/adinfo/rouge/server/page/SessionsPage.java
@@ -0,0 +1,48 @@
+package ca.qc.adinfo.rouge.server.page;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ca.qc.adinfo.rouge.RougeServer;
+import ca.qc.adinfo.rouge.server.core.SessionContext;
+import ca.qc.adinfo.rouge.server.core.SessionManager;
+import ca.qc.adinfo.rouge.server.servlet.RougePage;
+import ca.qc.adinfo.rouge.user.User;
+import ca.qc.adinfo.rouge.user.UserManager;
+
+public class SessionsPage extends RougePage {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		SessionManager sessionManager = RougeServer.getInstance().getSessionManager();
+
+		PrintWriter out = response.getWriter();
+
+		this.drawHeader(out);
+		this.startBox("Sessions", out);
+		this.startList(out);
+
+		for(SessionContext session: sessionManager.getSessions()) {
+
+			if (session.getUser() == null) {
+				out.println("<li><a href=\"/secure?action=session&id=" +session.getId() + "\">" + 
+						session.getId() + "</a>: </li>");
+			} else {
+				out.println("<li><a href=\"/secure?action=session&id=" +session.getId() + "\">" + 
+						session.getId() + "</a>: " + session.getUser().getUsername() + "</li>");
+			}
+		}
+
+		this.endList(out);
+		this.endBox(out);
+		this.drawFooter(out);
+	}
+
+}
