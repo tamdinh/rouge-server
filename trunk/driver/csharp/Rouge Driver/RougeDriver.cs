d5d3e0134a88c6bbb5768febad564efb5dadeaba
alexandre.denault@gmail.com <alexandre.denault@gmail.com@e1c9e452-2ef9-1ea6-95f3-d23a60559cc5>
2011-05-16 04:14:16 +0000

    First implementation of driver, JSon support, socket not yet working.

---
+++ trunk/driver/csharp/Rouge Driver/RougeDriver.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Rouge
+{
+    public class RougeDriver
+    {
+        private Socket socket;
+        private IPAddress hosta;
+        private IPEndPoint hostep;
+
+        private RougeListener listener;
+
+        public RougeDriver(String host, int port, RougeListener listener, bool bEncode)
+        {
+
+            this.listener = listener;
+
+            this.hosta = IPAddress.Parse("127.0.0.1");
+            this.hostep = new IPEndPoint(this.hosta, 6611);
+            this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);            
+        }
+
+        public void connect()
+        {
+            try
+            {
+                socket.Connect(hostep);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Problem connecting to host");
+                Console.WriteLine(e.ToString());
+                socket.Close();
+                return;
+            }
+        }
+
+        public void disconnect()
+        {
+            socket.Close();
+        }
+
+        public void send(String command, RougeObject payload)
+        {
+            Dictionary<string, object> toSend = new Dictionary<string, object>();
+            toSend.Add("command", command);
+            toSend.Add("payload", payload.toJSon());
+
+            string stringToSend = JsonConvert.SerializeObject(toSend) + "|" + "\n";
+
+            try
+            {
+                socket.Send(Encoding.ASCII.GetBytes(stringToSend));
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Problem sending data");
+                Console.WriteLine(e.ToString());
+                socket.Close();
+                return;
+            }
+
+            Console.WriteLine("Sent {0}", stringToSend);
+        }
+
+    }
+}
--- trunk/driver/csharp/Rouge Driver/RougeObject.cs
+++ trunk/driver/csharp/Rouge Driver/RougeObject.cs
@@ -2,10 +2,136 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Rouge
 {
     public class RougeObject
     {
+        private Dictionary<String, RougeDataWrapper> content;
+
+        public RougeObject()
+        {
+            this.content = new Dictionary<String, RougeDataWrapper>();
+        }
+
+        public RougeObject(String jSon)
+        {
+            this.content = new Dictionary<String, RougeDataWrapper>();
+
+            Dictionary<string, object> fromJson = JsonConvert.DeserializeObject<Dictionary<string, object>>(jSon);
+
+            foreach (KeyValuePair<String, object> keyValue in fromJson)
+            {
+                this.content.Add(keyValue.Key, new RougeDataWrapper(keyValue.Value));
+            }
+        }
+
+        public int getInt(String key)
+        {
+            return (int)this.content[key].getValue();
+        }
+
+        public long getLong(String key)
+        {
+            return (long)this.content[key].getValue();
+        }
+
+        public String getString(String key)
+        {
+            return (String)this.content[key].getValue();
+        }
+
+        public bool getBoolean(String key)
+        {
+            int b = (int)this.content[key].getValue();
+            return (b == 1);
+        }
+
+        public float getFloat(String key)
+        {
+            return (float)this.content[key].getValue();
+        }
+
+        public double getDouble(String key)
+        {
+            return (double)this.content[key].getValue();
+        }
+
+        public RougeArray getRougeArray(String key)
+        {
+            return (RougeArray)this.content[key].getValue();
+        }
+
+        public RougeObject getRougeObject(String key)
+        {
+            return (RougeObject)this.content[key].getValue();
+        }
+
+        public void put(String key, Object value)
+        {
+            this.content.Add(key, new RougeDataWrapper(value));
+        }
+
+        public void putInt(String key, int value)
+        {
+            this.content.Add(key, new RougeDataWrapper(value));
+        }
+
+        public void putLong(String key, long value)
+        {
+            this.content.Add(key, new RougeDataWrapper(value));
+        }
+
+        public void putString(String key, String value)
+        {
+            this.content.Add(key, new RougeDataWrapper(value));
+        }
+
+        public void putBoolean(String key, bool value)
+        {
+            if (value)
+            {
+                this.content.Add(key, new RougeDataWrapper(1));
+            }
+            else
+            {
+                this.content.Add(key, new RougeDataWrapper(0));
+            }
+        }
+
+        public void putFloat(String key, float value)
+        {
+            this.content.Add(key, new RougeDataWrapper(value));
+        }
+
+        public void putDouble(String key, double value)
+        {
+            this.content.Add(key, new RougeDataWrapper(value));
+        }
+
+        public void putRougeArray(String key, RougeArray value)
+        {
+            this.content.Add(key, new RougeDataWrapper(value));
+        }
+
+        public void putRougeObject(String key, RougeObject value)
+        {
+            this.content.Add(key, new RougeDataWrapper(value));
+        }
+
+        public String toJSon()
+        {
+
+            Dictionary<String, Object> toJson = new Dictionary<String, Object>();
+
+            foreach(KeyValuePair<String, RougeDataWrapper> keyValue in this.content) {
+                toJson.Add(keyValue.Key, keyValue.Value.getValue());
+            }
+
+            return JsonConvert.SerializeObject(toJson);
+        }
+
     }
 }
