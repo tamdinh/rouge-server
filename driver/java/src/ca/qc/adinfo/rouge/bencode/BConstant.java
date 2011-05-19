8910411c04e3e35289a78e90e008be8d17f1999a
alexandre.denault <alexandre.denault@gmail.com>
2011-05-19 04:26:55 +0000

    Implemented Bencode

---
+++ driver/java/src/ca/qc/adinfo/rouge/bencode/BConstant.java
@@ -0,0 +1,15 @@
+package ca.qc.adinfo.rouge.bencode;
+
+public class BConstant {
+
+	public static final char DICTIONARY_START = 'd';
+	public static final char DICTIONARY_END = 'e';
+
+	public static final char LIST_START = 'l';
+	public static final char LIST_END = 'e';
+	
+	public static final char NUMBER_START = 'i';
+	public static final char NUMBER_END = 'e';
+	
+	public static final char STRING_SEPERATOR = ':';
+}
