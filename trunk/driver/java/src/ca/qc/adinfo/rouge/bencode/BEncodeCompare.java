f6d204cc6afee16875e6f011371e9978427def90
alexandre.denault@gmail.com <alexandre.denault@gmail.com@e1c9e452-2ef9-1ea6-95f3-d23a60559cc5>
2011-05-19 04:26:55 +0000

    Implemented Bencode

---
+++ trunk/driver/java/src/ca/qc/adinfo/rouge/bencode/BEncodeCompare.java
@@ -0,0 +1,56 @@
+package ca.qc.adinfo.rouge.bencode;
+
+import java.io.ByteArrayInputStream;
+
+import ca.qc.adinfo.rouge.data.RougeArray;
+import ca.qc.adinfo.rouge.data.RougeObject;
+
+public class BEncodeCompare {
+	
+	public static void main(String[] args) {
+		
+		RougeObject ro = new RougeObject();
+		
+		compare(ro);
+		
+		ro.put("username", "password");
+		
+		compare(ro);
+		
+		ro.put("a", "value1");
+		ro.put("b", "value2");
+		
+		compare(ro);
+		
+		ro.put("c", 1234);
+		ro.put("d", 99);
+		
+		compare(ro);
+		
+		RougeArray array = new RougeArray();
+		
+		array.add("abc");
+		array.add(44);
+		array.add(45);
+		
+		ro.put("e", array);
+		
+		compare(ro);
+			
+	}
+	
+	public static void compare(RougeObject ro) {
+		
+		byte[] bCode = BEncoder.bencode(ro);
+		String jSon = ro.toJSON().toString();
+		System.out.println("BEncode : " + new String(bCode));
+		System.out.println("JSon    : " + jSon);
+		//System.out.println(BDecoder.bDecode(new ByteArrayInputStream(bCode)).toJSON().toString());
+		
+		System.out.println("JSon length " + jSon.length() + " BEncode length " + bCode.length + " reduction of " + (float)bCode.length/(float)jSon.length() );
+		
+		
+		
+	}
+
+}
