15db26d731befc62cdb37f20bd4d858f35bb7a64
alexandre.denault@gmail.com <alexandre.denault@gmail.com@e1c9e452-2ef9-1ea6-95f3-d23a60559cc5>
2011-05-21 05:09:50 +0000

    Stuff stored in DB don't need managers. More tweaks.

--- trunk/server/src/ca/qc/adinfo/rouge/achievement/Achievement.java
+++ trunk/server/src/ca/qc/adinfo/rouge/achievement/Achievement.java
@@ -3,6 +3,33 @@ package ca.qc.adinfo.rouge.achievement;
 
 public class Achievement {
 	
-	// TODO Implement Achievement
+	private String key;
+	private double progress;
+	private double max;
+	
+	public Achievement(String key, float max, float progress) {
+		
+		this.key = key;
+		this.max = max;
+		this.progress = this.progress;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+	
+	public double getProgress() {
+		return progress;
+	}
+	
+	public void updateProgress(double progress) {
+		this.progress = progress;
+	}
+	
+	public double getMax() {
+		return max;
+	}
+	
+	
 
 }
