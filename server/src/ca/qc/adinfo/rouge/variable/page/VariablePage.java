1b3edec23f7872115889c01288ab5b9386404a8a
alexandre.denault <alexandre.denault@gmail.com>
2011-05-23 15:02:52 +0000

    Removed warnings.

--- server/src/ca/qc/adinfo/rouge/variable/page/VariablePage.java
+++ server/src/ca/qc/adinfo/rouge/variable/page/VariablePage.java
@@ -1,19 +1,12 @@
 package ca.qc.adinfo.rouge.variable.page;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ca.qc.adinfo.rouge.RougeServer;
-import ca.qc.adinfo.rouge.monitor.ResourceTimePoint;
-import ca.qc.adinfo.rouge.room.RoomManager;
-import ca.qc.adinfo.rouge.server.core.SessionManager;
 import ca.qc.adinfo.rouge.server.servlet.RougeServerPage;
-import ca.qc.adinfo.rouge.user.UserManager;
 
 public class VariablePage extends RougeServerPage {
 
